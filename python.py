9a07d11cd2e976a3e33f2a00d7957ca2fcadaa0d
Djegui-lab <110122323+Djegui-lab@users.noreply.github.com>
2023-09-10 16:11:10 +0200

    Update python.py

--- python.py
+++ python.py
@@ -5,12 +5,17 @@ import sqlite3
 import seaborn as sns
 import matplotlib.pyplot as plt
 st.set_page_config(page_title='djégui', layout='wide')
-# Numéro de téléphone du cabinet de courtage
-numero_telephone = "0605275874"
 
-# Lien hypertexte pour appeler le numéro de téléphone
-lien_telephone = f"[Appeler le cabinet de courtage]({numero_telephone})"
-st.markdown(lien_telephone, unsafe_allow_html=True)
+# Titre de l'application
+st.title("Application d'Assurance Auto")
+
+# Numéro de téléphone du cabinet de courtage
+numero_telephone = "+1234567890"
+
+# Bouton pour appeler le numéro de téléphone
+if st.button(f"Appeler le cabinet de courtage ({numero_telephone})", key='call_button'):
+    st.write("Composez l'appel en utilisant votre téléphone.")
+
 
 # Saisie du nom de l'utilisateur
 user_name = st.text_input("Entrez votre nom et prénom")
